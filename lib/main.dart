703a5c5459ab5f2c29da426e2782908d6c27e55e
tyange <usun16@gmail.com>
2023-07-26 07:54:29 +0900

    making expense model

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_complete_expense_tracker/expenses.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Expenses(),
+    ),
+  );
+}
