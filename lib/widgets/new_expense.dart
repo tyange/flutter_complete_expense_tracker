3bc56bf652be9d1dec538ae7acd684bb0235610a
tyange <usun16@gmail.com>
2023-07-29 17:20:58 +0900

    making validation for submitted data

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -34,6 +34,16 @@ class _NewExpenseState extends State<NewExpense> {
     });
   }
 
+  void _submittedData() {
+    final enteredAmount = double.tryParse(
+      _amountController.text,
+    ); // tryParse('Hello') => null
+    final amountIsInvalid = enteredAmount == null || enteredAmount <= 0;
+    if (_titleController.text.trim().isEmpty ||
+        amountIsInvalid ||
+        _selectedCategory == null) {}
+  }
+
   @override
   void dispose() {
     _titleController.dispose();
@@ -119,10 +129,7 @@ class _NewExpenseState extends State<NewExpense> {
                   child: const Text('Cancel'),
                 ),
                 ElevatedButton(
-                  onPressed: () {
-                    print(_titleController.text);
-                    print(_amountController.text);
-                  },
+                  onPressed: _submittedData,
                   child: const Text('Save Expense'),
                 ),
               ],
