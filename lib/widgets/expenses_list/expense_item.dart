9a85dc6f0cc9d0adbd0a2a82d3507acf60004d10
tyange <usun16@gmail.com>
2023-07-29 10:30:04 +0900

    added icons & formatted date getter

--- lib/widgets/expenses_list/expense_item.dart
+++ lib/widgets/expenses_list/expense_item.dart
@@ -28,14 +28,14 @@ class ExpenseItem extends StatelessWidget {
               const Spacer(),
               Row(
                 children: [
-                  const Icon(
-                    Icons.alarm,
+                  Icon(
+                    categoryIcons[expense.category],
                   ),
                   const SizedBox(
                     width: 8,
                   ),
                   Text(
-                    expense.date.toString(),
+                    expense.formattedDate,
                   ),
                 ],
               )
