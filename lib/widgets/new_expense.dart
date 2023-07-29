456712d471ac509dc7d55cbcfeb41781c991f687
tyange <usun16@gmail.com>
2023-07-29 22:10:48 +0900

    made can disappear new expense widget when submitted

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -73,6 +73,7 @@ class _NewExpenseState extends State<NewExpense> {
         category: _selectedCategory,
       ),
     );
+    Navigator.pop(context);
   }
 
   @override
@@ -85,7 +86,12 @@ class _NewExpenseState extends State<NewExpense> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-        padding: const EdgeInsets.all(16),
+        padding: const EdgeInsets.fromLTRB(
+          16,
+          48,
+          16,
+          16,
+        ),
         child: Column(
           children: [
             TextField(
