ec60f4f05ad0af4f3a02738c8b73df663bfe902b
tyange <usun16@gmail.com>
2023-07-29 23:48:51 +0900

    add some theming

--- lib/widgets/expenses_list/expense_item.dart
+++ lib/widgets/expenses_list/expense_item.dart
@@ -15,33 +15,39 @@ class ExpenseItem extends StatelessWidget {
           horizontal: 20,
           vertical: 16,
         ),
-        child: Column(children: [
-          Text(expense.title),
-          const SizedBox(
-            height: 4,
-          ),
-          Row(
-            children: [
-              Text(
-                '\$${expense.amount.toStringAsFixed(2)}',
-              ),
-              const Spacer(),
-              Row(
-                children: [
-                  Icon(
-                    categoryIcons[expense.category],
-                  ),
-                  const SizedBox(
-                    width: 8,
-                  ),
-                  Text(
-                    expense.formattedDate,
-                  ),
-                ],
-              )
-            ],
-          )
-        ]),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              expense.title,
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+            const SizedBox(
+              height: 4,
+            ),
+            Row(
+              children: [
+                Text(
+                  '\$${expense.amount.toStringAsFixed(2)}',
+                ),
+                const Spacer(),
+                Row(
+                  children: [
+                    Icon(
+                      categoryIcons[expense.category],
+                    ),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    Text(
+                      expense.formattedDate,
+                    ),
+                  ],
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
